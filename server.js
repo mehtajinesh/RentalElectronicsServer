a8f6f2c10439aae9f7dd3989b46861c2fd65b945
Heekyung Kim <33532529+hkimkim@users.noreply.github.com>
2022-05-01 19:22:14 -0400

    first push

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const app = express();
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 mongoose.connect(CONNECTION_STRING);
 
+// adding to comment to test
+
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true
